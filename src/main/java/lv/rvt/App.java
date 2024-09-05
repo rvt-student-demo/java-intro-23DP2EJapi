aff6ad3bd1d661c325f3a4be16b53fda97c6365c
23DP2EJapi <166109765+23DP2EJapi@users.noreply.github.com>
2024-09-05 10:07:15 +0000

    projekts 0

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,11 +1,22 @@
 package lv.rvt;
 
+import java.util.Scanner;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        // Make your first Hello World app!
-        System.out.println("Hello World");
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Uzraksti savu vārdu:");
+        String vards = scanner.nextLine();
+        System.out.println("Uzraksti savu Uzvārdu:");
+        String uzvards = scanner.nextLine();
+        System.out.println("Uzraksti savu Grupu:");
+        String grupa = scanner.nextLine();
+
+        System.out.println("Jūs Uzrakstijāt: " + vards + " " + uzvards + " " + grupa);
+
 
     }
 
