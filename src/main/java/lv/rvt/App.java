510b7c8bbd2088a668cd18282dbe64f915c09445
23DP2EJapi <166109765+23DP2EJapi@users.noreply.github.com>
2024-09-11 09:33:49 +0000

    Uzdevums 1

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,28 +1,42 @@
 package lv.rvt;
 
-import java.util.Scanner;
+import java.util.*;
+
 
 public class App 
 {
     public static void main( String[] args )
     {
-        int vistas = 3;
-        double bekons = 5.5;
-        String traktors = "None!";
-        System.out.println("Chicken:");
-        System.out.println(vistas);
-        System.out.println("Bacon (kg):");
-        System.out.println(bekons);
-        System.out.println("Tractor:");
-        System.out.println(traktors);
-        System.out.println("");
-        System.out.println("And finally, a summary:");
-        System.out.println(vistas);
-        System.out.println(bekons);
-        System.out.println(traktors);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ierakstie savu vārdu:");
+
+        String vard = scanner.nextLine();
+
+        System.out.println("Ierakstiet savu uzvārdu:");
+
+        String uzvards = scanner.nextLine();
+
+        System.out.println("Ierakstiet savu grupu:");
+
+        String grupa = scanner.nextLine();
+
+        System.out.println("Students:" + vard + " " + uzvards + ", " + grupa);
 
 
+        /*String userInput = scanner.nextLine();
 
+        System.out.println(userInput);*/
+        /*String nname = "Janis";
+
+        System.out.println("Hello " + nname + " Wellcome!");
+
+        String start = "My name is ";
+        String end = ", James Bond";
+
+        System.out.println(start + "Bond" + end);
+
+        System.out.println("Mani euro: " + 90 + 100);*/
 
     }
 
