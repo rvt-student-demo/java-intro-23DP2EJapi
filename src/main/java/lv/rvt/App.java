0ff2110e49d1a1df244839b5c22bd9b59121413c
23DP2EJapi <166109765+23DP2EJapi@users.noreply.github.com>
2024-09-12 08:28:27 +0000

    Addition formula

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,36 +7,17 @@ public class App
 {
     public static void main( String[] args )
     {
+      
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Ierakstie savu vārdu:");
-
-        String vard = scanner.nextLine();
-
-        System.out.println("Ierakstiet savu uzvārdu:");
-
-        String uzvards = scanner.nextLine();
-
-        System.out.println("Ierakstiet savu grupu:");
-
-        String grupa = scanner.nextLine();
-
-        System.out.println("Students:" + vard + " " + uzvards + ", " + grupa);
+        System.out.println("Ievadiet 1 skaitli");
+        int pirmais = Integer.valueOf(scanner.nextLine());
+        System.out.println("Ievadiet 2 skaitli");
+        int otrais = Integer.valueOf(scanner.nextLine());
+        System.out.println(pirmais + " + " + otrais + " = " + (pirmais +otrais));
 
 
-        /*String userInput = scanner.nextLine();
 
-        System.out.println(userInput);*/
-        /*String nname = "Janis";
-
-        System.out.println("Hello " + nname + " Wellcome!");
-
-        String start = "My name is ";
-        String end = ", James Bond";
-
-        System.out.println(start + "Bond" + end);
-
-        System.out.println("Mani euro: " + 90 + 100);*/
 
     }
 
