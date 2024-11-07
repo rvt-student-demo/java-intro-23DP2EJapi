ed4eee63662742475d128368847125d158ad04df
23DP2EJapi <166109765+23DP2EJapi@users.noreply.github.com>
2024-11-07 09:10:21 +0000

    Payment Card

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,9 +8,25 @@ public class App
     public static void main( String[] args )
     {
         Scanner scanner = new Scanner(System.in);
-        int ievade = 0;
+        PaymentCard paulsCard = new PaymentCard(20);
+        PaymentCard mattsCard = new PaymentCard(30);
+        paulsCard.eatHeartily();
+        mattsCard.eatAffordably();
+        System.out.println("Paul:" + paulsCard);
+        System.out.println("Matt:" + mattsCard);
+        paulsCard.addMoney(20);
+        mattsCard.eatHeartily();
+        System.out.println("Paul:" + paulsCard);
+        System.out.println("Matt:" + mattsCard);
+        paulsCard.eatAffordably();
+        paulsCard.eatAffordably();
+        mattsCard.addMoney(50);
+        System.out.println("Paul:" + paulsCard);
+        System.out.println("Matt:" + mattsCard);
+
+        /*int ievade = 0;
  
-        Statistics statistics = new Statistics();
+        Statistics statistics = new Statistics();*/
         /*statistics.addNumber(3);
         statistics.addNumber(5);
         statistics.addNumber(1);
@@ -19,7 +35,7 @@ public class App
         System.out.println("Sum: " + statistics.sum());
         System.out.println("Average: " + statistics.average());*/
 
-        while(true){
+        /*while(true){
             
             ievade = Integer.valueOf(scanner.nextLine());
             if (ievade == -1) {
@@ -31,7 +47,7 @@ public class App
         }
         System.out.println("Sum: " + statistics.sum());
         System.out.println("Sum of even numbers: " + statistics.sumeven());
-        System.out.println("Sum of ood numbers: " + statistics.sumodd());
+        System.out.println("Sum of ood numbers: " + statistics.sumodd());*/
     }
 
 
