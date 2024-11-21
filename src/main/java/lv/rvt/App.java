f59cd9ca89088e05a95570b7ea670151ba6e00f0
EJJ <a230739ej@rvt.lv>
2024-11-21 13:04:44 +0200

    Constructor Overload

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -10,18 +10,21 @@ import java.nio.file.StandardOpenOption;
 
 public class App {
     public static void main( String[] args) {
+        Product tapeMeasure = new Product("Tape measure");
+        Product plaster = new Product("Plaster", "home improvement section");
+        Product tyre = new Product("Tyre", 5);
 
-        Timer timer = new Timer();
+        System.out.println(tapeMeasure);
+        System.out.println(plaster);
+        System.out.println(tyre);
 
-        while (true) {
-            System.out.println(timer);
-            timer.advance();
         
-            try {
-                Thread.sleep(10);
-            } catch (Exception e) {
-        
-            }
-        }
     }
+
+
+
+
+
+
+
 }
